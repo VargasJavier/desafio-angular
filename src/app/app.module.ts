63310d9b618ce528db0938ee637fcb8954588822
Javier Vargas <jabo94454@gmail.com>
2022-07-21 21:41:16 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { ItemComponent } from './components/item/item.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ItemListComponent } from './components/item-list/item-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    ItemComponent,
+    MenuComponent,
+    ItemListComponent
   ],
   imports: [
     BrowserModule
